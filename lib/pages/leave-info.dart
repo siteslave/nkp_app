636dca0b93f09bcfe78e50716b36924980034067
Satit Riapit <siteslave@msn.com>
2019-07-11 16:11:11 +0700

    add bottom button

--- lib/pages/leave-info.dart
+++ lib/pages/leave-info.dart
@@ -20,47 +20,119 @@ class _LeaveInfoPageState extends State<LeaveInfoPage> {
     String startDate = helper.toShortThaiDate(_startDate);
     String endDate = helper.toShortThaiDate(_endDate);
 
-    Widget _infoWidget = ListView(
+    Widget _infoWidget = Column(
       children: <Widget>[
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          height: 60,
-          margin: EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-              color: Colors.purple[100],
-              borderRadius: BorderRadius.circular(10)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        Expanded(
+          child: ListView(
             children: <Widget>[
-              Text('ประเภท'),
-              Text(
-                '${widget.leave['leave_type_name']}',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              )
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 60,
+                margin: EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.purple[100],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('ประเภท'),
+                    Text(
+                      '${widget.leave['leave_type_name']}',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 60,
+                margin: EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.purple[100],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('ช่วงเวลา'),
+                    Text(
+                      '$startDate - $endDate',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 60,
+                margin: EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.purple[100],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('จำนวนวันลา'),
+                    Text(
+                      '${widget.leave['leave_days']} วัน',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 60,
+                margin: EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.purple[100],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('เหตุผล'),
+                    Text(
+                      '${widget.leave['remark']}',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
-        Container(
-          height: 60,
-          margin: EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-              color: Colors.purple[100],
-              borderRadius: BorderRadius.circular(10)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text('ช่วงเวลา'),
-              Text(
-                '$startDate - $endDate',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: RaisedButton(
+                padding: EdgeInsets.all(20),
+                onPressed: () {},
+                child: Text(
+                  'อนุมัติ',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                color: Colors.green,
+              ),
+            ),
+            Expanded(
+              child: RaisedButton(
+                padding: EdgeInsets.all(20),
+                color: Colors.red,
+                onPressed: () {},
+                child: Text('ไม่อนุมัติ',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+              ),
+            ),
+          ],
+        )
       ],
     );
 
